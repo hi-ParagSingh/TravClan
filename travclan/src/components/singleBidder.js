edfd6f6189c48c10b05739b07c0a232539cd9c5c
Parag Singh <psparag1997@gmail.com>
2021-06-05 21:12:07 +0530

    User Image and Button added

--- travclan/src/components/singleBidder.js
+++ travclan/src/components/singleBidder.js
@@ -8,7 +8,9 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import TablePagination from "@material-ui/core/TablePagination";
 import Paper from "@material-ui/core/Paper";
-
+import { Button } from '@material-ui/core';
+import arrow from '../assets/right.svg'
+import user from '../assets/user.png'
 const useStyles = makeStyles({
     table: {
         minWidth: 650
@@ -19,69 +21,55 @@ const SingleBidder = (props) => {
 
     var apiRes = [
         {
-            userId: 12,
+            userId: 101,
+            imageSrc: user,
             firstName: 'Parag',
             lastName: "Singh",
             email: "psparag1997@gmail.com",
             phone: "+918295921803",
-            hasPremium: false,
+            hasPremium: true,
             bids: [11111, 145555, 1121212]
         },
         {
-            userId: 12,
+            userId: 102,
+            imageSrc: user,
             firstName: 'Parag',
             lastName: "Singh",
             email: "psparag1997@gmail.com",
             phone: "+918295921803",
-            hasPremium: false,
+            hasPremium: true,
             bids: [11111, 145555, 1121212]
         },
         {
-            userId: 12,
+            userId: 103,
+            imageSrc: user,
             firstName: 'Parag',
             lastName: "Singh",
             email: "psparag1997@gmail.com",
             phone: "+918295921803",
-            hasPremium: false,
+            hasPremium: true,
             bids: [11111, 145555, 1121212]
         },
         {
-            userId: 12,
+            userId: 104,
+            imageSrc: user,
             firstName: 'Parag',
             lastName: "Singh",
             email: "psparag1997@gmail.com",
             phone: "+918295921803",
-            hasPremium: false,
+            hasPremium: true,
             bids: [11111, 145555, 1121212]
         },
         {
-            userId: 12,
+            userId: 105,
+            imageSrc: user,
             firstName: 'Parag',
             lastName: "Singh",
             email: "psparag1997@gmail.com",
             phone: "+918295921803",
-            hasPremium: false,
+            hasPremium: true,
             bids: [11111, 145555, 1121212]
         },
-        {
-            userId: 12,
-            firstName: 'Parag',
-            lastName: "Singh",
-            email: "psparag1997@gmail.com",
-            phone: "+918295921803",
-            hasPremium: false,
-            bids: [11111, 145555, 1121212]
-        },
-        {
-            userId: 12,
-            firstName: 'Parag',
-            lastName: "Singh",
-            email: "psparag1997@gmail.com",
-            phone: "+918295921803",
-            hasPremium: false,
-            bids: [11111, 145555, 1121212]
-        },
-
     ];
 
     const classes = useStyles();
@@ -105,30 +93,34 @@ const SingleBidder = (props) => {
                 <TableHead>
                     <TableRow>
                         <TableCell>User ID</TableCell>
+                        <TableCell>Image</TableCell>
                         <TableCell align="left">First Name</TableCell>
                         <TableCell align="left">Last Name</TableCell>
                         <TableCell align="left">Email</TableCell>
                         <TableCell align="left">Contact</TableCell>
                         <TableCell align="left">Premium Member</TableCell>
-                        <TableCell align="left">{props.toogleState? 'Highest Bid': 'Lowest Bid'}</TableCell>
+                        <TableCell align="left">{props.toogleState ? 'Highest Bid' : 'Lowest Bid'}</TableCell>
+                        <TableCell align="left">All Bids</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {apiRes
-                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                    .map((row, index) => (
-                        <TableRow key={row.name}>
-                            <TableCell component="th" scope="row">
-                                {row.userId}
-                            </TableCell>
-                            <TableCell align="left">{row.firstName}</TableCell>
-                            <TableCell align="left">{row.lastName}</TableCell>
-                            <TableCell align="left">{row.email}</TableCell>
-                            <TableCell align="left">{row.phone}</TableCell>
-                            <TableCell align="left">{row.hasPremium.toString()}</TableCell>
-                            <TableCell align="left">{props.toogleState? Math.max(...row.bids): Math.min(...row.bids)}</TableCell>
-                        </TableRow>
-                    ))}
+                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                        .map((row, index) => (
+                            <TableRow key={row.name}>
+                                <TableCell component="th" scope="row">
+                                    {row.userId}
+                                </TableCell>
+                                <TableCell align="left"><img src={row.imageSrc} alt="" srcset="" className="userImage"/></TableCell>
+                                <TableCell align="left">{row.firstName}</TableCell>
+                                <TableCell align="left">{row.lastName}</TableCell>
+                                <TableCell align="left">{row.email}</TableCell>
+                                <TableCell align="left">{row.phone}</TableCell>
+                                <TableCell align="left">{row.hasPremium.toString().toUpperCase()}</TableCell>
+                                <TableCell align="left">{props.toogleState ? Math.max(...row.bids) : Math.min(...row.bids)}</TableCell>
+                                <TableCell align="left"><Button variant="text"><img src={arrow} alt="" /></Button></TableCell>
+                            </TableRow>
+                        ))}
                 </TableBody>
             </Table>
             <TablePagination

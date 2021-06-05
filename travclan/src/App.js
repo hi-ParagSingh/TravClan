b7dcec1e2e76f28ced2c1a3e297a70d59c03cb7f
Parag Singh <psparag1997@gmail.com>
2021-06-05 20:49:04 +0530

    Added Table for Table Pagination

--- travclan/src/App.js
+++ travclan/src/App.js
@@ -1,66 +1,12 @@
-import Bidder from "./components/singleBidder"
+import Bidder from "./components/SingleBidder"
 import React, { useEffect } from "react";
-import { Switch, Pagination, Stack } from '@material-ui/core';
+import { Switch } from '@material-ui/core';
+
+
 
 function App() {
-  var apiRes = [
-    {
-      userId: 12,
-      firstName: 'Parag',
-      lastName: "Singh",
-      email: "psparag1997@gmail.com",
-      phone: "+918295921803",
-      hasPremium: false,
-      bids: [11111, 145555, 1121212]
-    },
-    {
-      userId: 12,
-      firstName: 'Parag',
-      lastName: "Singh",
-      email: "psparag1997@gmail.com",
-      phone: "+918295921803",
-      hasPremium: false,
-      bids: [11111, 145555, 1121212]
-    },
-    {
-      userId: 12,
-      firstName: 'Parag',
-      lastName: "Singh",
-      email: "psparag1997@gmail.com",
-      phone: "+918295921803",
-      hasPremium: false,
-      bids: [11111, 145555, 1121212]
-    },
-    {
-      userId: 12,
-      firstName: 'Parag',
-      lastName: "Singh",
-      email: "psparag1997@gmail.com",
-      phone: "+918295921803",
-      hasPremium: false,
-      bids: [11111, 145555, 1121212]
-    },
-    {
-      userId: 12,
-      firstName: 'Parag',
-      lastName: "Singh",
-      email: "psparag1997@gmail.com",
-      phone: "+918295921803",
-      hasPremium: false,
-      bids: [11111, 145555, 1121212]
-    },
-    {
-      userId: 12,
-      firstName: 'Parag',
-      lastName: "Singh",
-      email: "psparag1997@gmail.com",
-      phone: "+918295921803",
-      hasPremium: false,
-      bids: [11111, 145555, 1121212]
-    },
-
-  ];
-
+  
+ 
   const [state, setState] = React.useState({
     checkedA: true,
   });
@@ -82,25 +28,15 @@ function App() {
       })
   }, []);
 
-
-
-
   return (
     <div className="App">
-      <div className="toggleButton">Toogle Price<Switch
+      <div className="toggleButton">Show Highest Bids<Switch
         checked={state.checkedA}
         onChange={handleChange}
         name="checkedA"
         inputProps={{ 'aria-label': 'secondary checkbox' }}
       /></div>
-      {apiRes.map((data, index) => (
-        <Bidder key={index} values={data} toogleState={state.checkedA}></Bidder>
-      ))}
-
-      <Stack spacing={2}>
-        <Pagination count={10} variant="outlined" shape="rounded" />
-      </Stack>
-
+      <Bidder toogleState={state.checkedA}></Bidder>
     </div>
   );
 }
--- travclan/src/components/singleBidder.js
+++ travclan/src/components/singleBidder.js
@@ -1,19 +1,148 @@
 import React from 'react'
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TablePagination from "@material-ui/core/TablePagination";
+import Paper from "@material-ui/core/Paper";
 
-const singleBidder = (props) => {
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650
+    }
+});
+
+const SingleBidder = (props) => {
+
+    var apiRes = [
+        {
+            userId: 12,
+            firstName: 'Parag',
+            lastName: "Singh",
+            email: "psparag1997@gmail.com",
+            phone: "+918295921803",
+            hasPremium: false,
+            bids: [11111, 145555, 1121212]
+        },
+        {
+            userId: 12,
+            firstName: 'Parag',
+            lastName: "Singh",
+            email: "psparag1997@gmail.com",
+            phone: "+918295921803",
+            hasPremium: false,
+            bids: [11111, 145555, 1121212]
+        },
+        {
+            userId: 12,
+            firstName: 'Parag',
+            lastName: "Singh",
+            email: "psparag1997@gmail.com",
+            phone: "+918295921803",
+            hasPremium: false,
+            bids: [11111, 145555, 1121212]
+        },
+        {
+            userId: 12,
+            firstName: 'Parag',
+            lastName: "Singh",
+            email: "psparag1997@gmail.com",
+            phone: "+918295921803",
+            hasPremium: false,
+            bids: [11111, 145555, 1121212]
+        },
+        {
+            userId: 12,
+            firstName: 'Parag',
+            lastName: "Singh",
+            email: "psparag1997@gmail.com",
+            phone: "+918295921803",
+            hasPremium: false,
+            bids: [11111, 145555, 1121212]
+        },
+        {
+            userId: 12,
+            firstName: 'Parag',
+            lastName: "Singh",
+            email: "psparag1997@gmail.com",
+            phone: "+918295921803",
+            hasPremium: false,
+            bids: [11111, 145555, 1121212]
+        },
+        {
+            userId: 12,
+            firstName: 'Parag',
+            lastName: "Singh",
+            email: "psparag1997@gmail.com",
+            phone: "+918295921803",
+            hasPremium: false,
+            bids: [11111, 145555, 1121212]
+        },
+
+    ];
+
+    const classes = useStyles();
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = event => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+    const emptyRows =
+        rowsPerPage - Math.min(rowsPerPage, apiRes.length - page * rowsPerPage);
+
+
     return (
-
-        <div className="singleBidder">
-            <div className="userId">{props.values.userId}</div>
-            <div className="firstName">{props.values.firstName}</div>
-            <div className="lastName">{props.values.lastName}</div>
-            <div className="email">{props.values.email}</div>
-            <div className="phone">{props.values.phone}</div>
-            <div className="hasPremium">{props.values.hasPremium}</div>
-            <div className="bids">{props.toogleState ? Math.max(...props.values.bids) : Math.min(...props.values.bids)}</div>
-        </div>
+        <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>User ID</TableCell>
+                        <TableCell align="left">First Name</TableCell>
+                        <TableCell align="left">Last Name</TableCell>
+                        <TableCell align="left">Email</TableCell>
+                        <TableCell align="left">Contact</TableCell>
+                        <TableCell align="left">Premium Member</TableCell>
+                        <TableCell align="left">{props.toogleState? 'Highest Bid': 'Lowest Bid'}</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {apiRes
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((row, index) => (
+                        <TableRow key={row.name}>
+                            <TableCell component="th" scope="row">
+                                {row.userId}
+                            </TableCell>
+                            <TableCell align="left">{row.firstName}</TableCell>
+                            <TableCell align="left">{row.lastName}</TableCell>
+                            <TableCell align="left">{row.email}</TableCell>
+                            <TableCell align="left">{row.phone}</TableCell>
+                            <TableCell align="left">{row.hasPremium.toString()}</TableCell>
+                            <TableCell align="left">{props.toogleState? Math.max(...row.bids): Math.min(...row.bids)}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+            <TablePagination
+                rowsPerPageOptions={[5, 10, 25]}
+                component="div"
+                count={apiRes.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+            />
+        </TableContainer>
 
     )
 }
 
-export default singleBidder
+export default SingleBidder
